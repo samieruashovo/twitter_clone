d39daad91986acd875bb9b8ce634d4c5145dd638
Samier Shovo <84716783+samieruashovo@users.noreply.github.com>
2023-07-29 11:18:46 +0600

    Added twitter_clone folder

---
+++ frontend/twitter_clone/src/App.js
@@ -0,0 +1,9 @@
+import React, { useEffect } from "react";
+import "./App.css";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+
+function App() {
+  return <div></div>;
+}
+
+export default App;
